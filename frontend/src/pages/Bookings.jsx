7853e6b44ce4e7afe7a636812a97ae181b93762c
mogilisaibalaji <saibalajimogili@gmail.com>
2025-10-29 22:55:42 +0530

    init

--- frontend/src/pages/Bookings.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Bookings(){
-    return(
-        <h2>Add Bookings</h2>
-    )
-}
--- frontend/src/pages/Students.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function Students(){
-    return (
-        <h2>welcome to home page </h2>
-    )
-}
